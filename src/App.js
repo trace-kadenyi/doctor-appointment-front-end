4e23f266705fad44881e27b3e118d1c4f8a2b0d7
Abby Nyakara <abbynyakara@gmail.com>
2022-11-28 16:43:31 +0300

    add create store & install packages

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello!</h1>
-    </div>
+    </div>   
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import store from './Redux/store';
+import Provider from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
