488ddc73aae74e10d0e8d702648c597a5fd673e2
kadenyi <treykadenyi@gmail.com>
2022-11-28 22:58:08 +0300

    Modify store to add rootreducer and add the doctors reducer

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,9 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import doctorSlice from '../Redux/doctorSlice';
+
+const rootReducer = {
+  doctors: doctorSlice,
+};
 
 const store = configureStore({
-  reducer: {
-
-  },
+  reducer: rootReducer,
 });
 
 export default store;
