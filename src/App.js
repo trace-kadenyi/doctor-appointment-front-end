3b8a8d12e8639655a0b1dd608befca4abce8f98d
kadenyi <treykadenyi@gmail.com>
2022-11-28 22:27:18 +0300

    Remove Routes and Home component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './components/Home';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route to="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <div>App</div>
   );
 }
 
