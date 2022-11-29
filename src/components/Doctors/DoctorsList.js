ebbd59523d363286866ebf729678de9384a316e5
kadenyi <treykadenyi@gmail.com>
2022-11-29 23:30:07 +0300

    Display doctors' list

--- src/components/Doctors/DoctorsList.js
+++ src/components/Doctors/DoctorsList.js
@@ -1,8 +1,11 @@
 /* eslint-disable */
 
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchDoctors, doctorSelector } from '../../Redux/doctorSlice';
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { BiRightArrow, BiLeftArrow } from "react-icons/bi";
+import { fetchDoctors, doctorSelector } from "../../Redux/doctorSlice";
+
+import "./doctors.css";
 
 const DoctorsList = () => {
   const dispatch = useDispatch();
@@ -14,22 +17,43 @@ const DoctorsList = () => {
   }, [dispatch]);
 
   return (
-    <div>
-      <h1>Doctors</h1>
+    <div className="doctors_sect">
       {/* loading main page */}
-      {doctors.loading && <div className='loading'>Loading...</div>}
+      {doctors.loading && <div className="loading">Loading...</div>}
       {/* error main page */}
-      {doctors.hasErrors && <div className='error'>Unable to display doctors.</div>}
+      {doctors.hasErrors && (
+        <div className="error">Unable to display doctors.</div>
+      )}
       {/* doctors list */}
-      {doctors.doctors.map((doctor) => (
-        <div key={doctor.id} className="doctors_div">
-          <h2 className='doctors_name'>{doctor.name}</h2>
-          <p className='specialization'>{doctor.specialization}</p>
+      <div className="content_div">
+        <div className="arrow_div">
+          <button className="arrow left">
+            <BiLeftArrow />
+          </button>
+        </div>
+        <div className="cover_div">
+          <div className="scroll_content">
+            {doctors.doctors.map((doctor) => (
+              <div key={doctor.id} className="doctors_div">
+                <img
+                  className="doctors_img"
+                  src={doctor.photo}
+                  alt={doctor.name}
+                />
+                <h2 className="doctors_name">{doctor.name}</h2>
+                <p className="specialization">{doctor.specialization}</p>
+              </div>
+            ))}
           </div>
-      ))}
-      
+        </div>
+        <div>
+          <button className="arrow">
+            <BiRightArrow />
+          </button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default DoctorsList;
