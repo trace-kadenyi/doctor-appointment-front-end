013b446804c3a8683acb94b129951dc35d3a0304
brahimdidi <brahimgb1team@gmail.com>
2022-11-29 15:33:35 +0000

    fetch all users in user slice

---
+++ src/Redux/UserReducer.js
@@ -0,0 +1,46 @@
+  import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+  const UserApi = "http://localhost:3000/api/v1/users";
+
+  // fetch all users for smoother login 
+  export const fetchUsers = createAsyncThunk('user/getUsers', async () => {
+    const res = await fetch(UserApi);
+    const data = res.json();
+    const users = await data;
+    return users;
+  });
+  
+  
+
+  const options = {
+    name: 'User',
+    initialState: {
+      users: [],
+      currentUser: {}
+    },
+    reducers: {
+
+    },
+    extraReducers: {
+      [fetchUsers.fulfilled]: (state, action) => { 
+        state.users = action.payload;
+        // this is just for testing purposes users shouldn't be public.
+        localStorage.removeItem('users');
+        localStorage.setItem('users', JSON.stringify(action.payload));
+      },
+      [fetchCreateUser.rejected]: (state,action) => {
+        // render the error
+      },
+      [fetchCreateUser.fulfilled]: (state,action) => {
+        state.currentUser = action.payload
+        localStorage.setItem('currentUser', JSON.stringify(action.payload));
+      }
+    },
+  };
+
+  export const  UserSlice = createSlice(options);
+  export const {setCurrentUser} = UserSlice.actions;
+  export default UserSlice.reducer;
+  export const selectUsers = (state) => state.user.users;
+  export const selectCurrentUser = (state) => state.user.currentUser;
+  export const selectError = (state) => state.user.error;
