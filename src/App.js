629698dfbef4a25bac3c2943ea2bcb813f391c58
brahimdidi <brahimgb1team@gmail.com>
2022-11-29 15:46:45 +0000

    if user logged in, set the current user in the state.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
+import User from './components/User';
+import { setCurrentUser } from './Redux/UserReducer';
+import { useDispatch } from 'react-redux';
+
 function App() {
+  const dispatch = useDispatch();
+  const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+  // if the user is logged in copy users data from the local storage to redux state.
+  if(currentUser) { console.log('bamo'); dispatch(setCurrentUser(currentUser))};
   return (
     <Router>
       <Routes>
-        <Route path="/" element={ <Home/> } />
+        { currentUser  &&  <Route path="/home" element={ <Home/> } />
+        }
+        { !currentUser && <Route path="/" element={ <User/> } /> }
       </Routes>
     </Router>
   );
