c69f493971a67d9ed17ceca2a2158dcadb1235a2
Abby Nyakara <abbynyakara@gmail.com>
2022-11-30 13:51:43 +0300

    create responsive navbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DoctorsList from './components/Doctors/DoctorsList';
+import Navigation from './components/Nav/Navigation';
 
 function App() {
   return (
     <Router>
+      <Navigation />
       <Routes>
         <Route exact path="/" element={<DoctorsList />} />
       </Routes>
