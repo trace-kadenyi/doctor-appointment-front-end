f27750ff61abb21f758ec1751d2805d68a986dd0
Brahim didi <brahimgb1team@gmail.com>
2022-11-30 11:19:15 +0000

    remove users from localstorage. add selectors for pending and rejected.

--- src/Redux/UserReducer.js
+++ src/Redux/UserReducer.js
@@ -1,97 +1,68 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import React, { useState, useEffect } from 'react';
 
-const UserApi = 'http://localhost:3000/api/v1/users';
+import { useDispatch, useSelector } from 'react-redux';
+import {
+  fetchCreateUser, fetchUsers, selectPending, selectRejected, selectUsers, setCurrentUser,
+} from '../Redux/UserReducer';
+function User() {
+  const dispatch = useDispatch();
+  const users = useSelector(selectUsers);
 
-// fetch all users for smoother login
-export const fetchUsers = createAsyncThunk('user/getUsers', async () => {
-  const res = await fetch(UserApi);
-  const data = res.json();
-  const users = await data;
-  return users;
-});
+  // get the input value
+  const [username, setUsername] = useState('');
+  // fetch users once.
+  useEffect(() => {
+    if (!users.length) { dispatch(fetchUsers()); }
+  }, [dispatch]);
 
-// Create user on sign up params {name: ''}
-export const fetchCreateUser = createAsyncThunk('user/signup', async (user) => fetch(UserApi, {
-  method: 'POST',
-  headers: {
-    'Content-type': 'application/json',
-  },
-  body: JSON.stringify(user),
-}).then(async (res) => {
-  const data = res.json();
-  const user = await data;
-  return user;
-}));
+  // log in the user
+  const loginUser = (name) => {
+    const currentUser = users.filter((e) => e.name === name);
+    if (currentUser.length) {
+      // user is logged in render the home page, notify the user.
+      console.log('logged in');
+      dispatch(setCurrentUser(currentUser[0]));
+      localStorage.setItem('currentUser', JSON.stringify(currentUser[0]));
+    } else {
+      // notify the message
+      console.log('username does not exists');
+      document.getElementById('username-input').value = '';
+    }
+  };
+  // sign up the user
+  const signUp = (name) => {
+    // check locally if the name already exists to avoid useless api call.
+    const currentUser = users.filter((e) => e.name === name);
+    if (currentUser.length) {
+      // notify the message
+      console.log('username already exists, chose another one or login');
+      document.getElementById('username-input').value = '';
+    } else {
+      // user is signed up
+      // render the home page and notify the user.
+      console.log(`signed up ${name}`);
+      dispatch(fetchCreateUser({ name }));
+    }
+  };
 
-const options = {
-  name: 'User',
-  initialState: {
-    users: [],
-    currentUser: {},
-    error: '',
-    pending: false,
-    rejected: false,
-    fulfilled: false,
-  },
-  reducers: {
-    setCurrentUser(state, action) {
-      const currentUserState = state;
-      currentUserState.currentUser = action.payload;
-    },
-    signOut(state) {
-      // notify the user that he was signed out
-      // rerender the login page.
-      const currentUserState = state;
-      console.log('signed out');
-      currentUserState.currentUser = {};
-      localStorage.removeItem('currentUser');
-    },
-  },
-  extraReducers: {
-    [fetchUsers.pending]: (state) => {
-      const pendingState = state;
-      pendingState.pending = true;
-    },
-    [fetchUsers.rejected]: (state, action) => {
-      const rejectedState = state;
-      // render the error
-      rejectedState.error = action.payload;
-      rejectedState.pending = false;
-      rejectedState.rejected = true;
-    },
-    [fetchUsers.fulfilled]: (state, action) => {
-      const fulfilledState = state;
-      fulfilledState.users = action.payload;
-      fulfilledState.fulfilled = true;
-      fulfilledState.pending = false;
-      // this is just for testing purposes users shouldn't be public.
-      localStorage.removeItem('users');
-      localStorage.setItem('users', JSON.stringify(action.payload));
-    },
-    [fetchCreateUser.pending]: (state) => {
-      const pendingState = state;
-      pendingState.pending = true;
-    },
-    [fetchCreateUser.rejected]: (state, action) => {
-      const rejectedState = state;
-      // render the error
-      rejectedState.rejected = true;
-      rejectedState.pending = false;
-      rejectedState.error = action.payload;
-    },
-    [fetchCreateUser.fulfilled]: (state, action) => {
-      const fulfilledState = state;
-      fulfilledState.fulfilled = true;
-      fulfilledState.pending = false;
-      fulfilledState.currentUser = action.payload;
-      localStorage.setItem('currentUser', JSON.stringify(action.payload));
-    },
-  },
-};
-
-export const UserSlice = createSlice(options);
-export const { setCurrentUser, signOut } = UserSlice.actions;
-export default UserSlice.reducer;
-export const selectUsers = (state) => state.user.users;
-export const selectCurrentUser = (state) => state.user.currentUser;
-export const selectAll = (state) => state.user;
+  return (
+    <section className="login-section">
+      { useSelector(selectPending) && (
+        'loading'
+      )}
+       { useSelector(selectRejected) && (
+        'something went wrong, check your internet again. '
+      )}
+      <form>
+        <p>Enter your username , You can chose to either login or sign up, no password required.</p>
+        <div>
+          <input id="username-input" placeholder="enter your username" onChange={(e) => setUsername(e.target.value)} />
+          <br />
+          <button type="submit" onClick={(e) => { e.preventDefault(); loginUser(username); }}>log in</button>
+          <button type="submit" onClick={(e) => { e.preventDefault(); signUp(username); }}>sign up</button>
+        </div>
+      </form>
+    </section>
+  );
+}
+export default User;
