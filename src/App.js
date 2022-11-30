5eda0d3f39dfd995fcce7c3df34c5f54da817813
brahimdidi <brahimgb1team@gmail.com>
2022-11-30 14:18:21 +0000

    add conditioning to the main route to log in user

--- src/App.js
+++ src/App.js
@@ -4,23 +4,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import Home from './components/Home';
 import User from './components/User';
-import { selectCurrentUser, setCurrentUser } from './Redux/UserReducer';
+import { setCurrentUser } from './Redux/UserReducer';
 import DoctorsList from './components/Doctors/DoctorsList';
 
 function App() {
   const dispatch = useDispatch();
-  const currentUserRedux = useSelector(selectCurrentUser);
-  const currentUserLStorage = JSON.parse(localStorage.getItem('currentUser'));
-  // check if a current user exists in local storage or in the redux store.
-  const currentUser = currentUserLStorage || currentUserRedux;
+  const currentUser = JSON.parse(localStorage.getItem('currentUser'));
   // if the user is logged in copy users data from the local storage to redux state.
   if (currentUser) { dispatch(setCurrentUser(currentUser)); }
   return (
     <Router>
       <Routes>
         <Route path="/home" element={<Home />} />
-        { !currentUser && <Route path="/" element={<User />} /> }
-        <Route exact path="/" element={<DoctorsList />} />
+        { !currentUser ? <Route path="/" element={<User />} /> : <Route exact path="/" element={<DoctorsList />} /> }
+        
       </Routes>
     </Router>
   );
