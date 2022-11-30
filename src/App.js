5a7036215568f71438f5ab792b26efbe7e45f6d8
Abby Nyakara <abbynyakara@gmail.com>
2022-11-30 20:20:37 +0300

    redesign nav

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DoctorsList from './components/Doctors/DoctorsList';
-import Navigation from './components/Nav/Navigation';
+// import Navigation from './components/Nav/Navigation';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <Router>
-      <Navigation />
+      <Navbar />
       <Routes>
         <Route exact path="/" element={<DoctorsList />} />
       </Routes>
