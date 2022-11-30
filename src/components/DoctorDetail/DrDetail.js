13f4cf764f2391f210bf5158d9885b9c09fcbc88
Aleksandra Ujvari <ujvari65@gmail.com>
2022-11-30 23:40:41 +0100

    Install bootstrap

--- src/components/DoctorDetail/DrDetail.js
+++ src/components/DoctorDetail/DrDetail.js
@@ -17,29 +17,31 @@ const DrDetail = () => {
 
   return (
     <section className="doctor-detail">
-      <div className="link_to_doctors">
-        <Link to="/">
-          <FaAngleLeft />
-          Back to doctors
-        </Link>
-      </div>
       <div className="doctor-detail__container">
-        <div className="doctor-detail__photo">
-          <img className="doctor_photo" src={doctor.photo} alt="doctor" />
+        <div className="link_to_doctors">
+          <Link to="/">
+            <FaAngleLeft />
+            Back to doctors
+          </Link>
         </div>
-        <div>
+        <div className="doctor-detail__photo">
+          <div className="photo">
+            <img className="doctor_photo" src={doctor.photo} alt="doctor" />
+          </div>
+        </div>
+        <div className="info__list">
           <ul className="doctor-detail__info">
             <li><h2 className="doctor-detail__name">{doctor.name}</h2></li>
-            <li><p className="doctor-detail__specializations">{doctor.specializations}</p></li>
+            <li><p className="doctor-detail__specializations">{doctor.specialization}</p></li>
             <li><Link to={`/doctors/${id}/appointments`}>Make an appointment</Link></li>
           </ul>
         </div>
-      </div>
-      <div className="link_to_doctors">
-        <Link to="/">
-          <FaAngleLeft />
-          Back to doctors
-        </Link>
+        <div className="link_to_doctors">
+          <Link to="/">
+            <FaAngleLeft />
+            Back to doctors
+          </Link>
+        </div>
       </div>
     </section>
   );
