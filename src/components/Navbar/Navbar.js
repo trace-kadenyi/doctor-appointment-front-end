bf6eb2a15091d2cbe9ddfac12d2c14fb3e2bd3b7
Abby Nyakara <abbynyakara@gmail.com>
2022-12-01 11:52:11 +0300

    add mobile version

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,7 +4,6 @@ import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../../assets/images/logo.png';
 import humburgerMenu from '../../assets/images/humburger-menu.svg';
-import closeMenu from '../../assets/images/close-icon.svg';
 import './navbar.css';
 
 const Navbar = () => {
@@ -18,33 +17,35 @@ const Navbar = () => {
             <img src={humburgerMenu} alt="Menu Icon" />
           </button>
 
-          <div className="navbar_left">
-            <NavLink className="nav__logo" to="/">
-              <img src={logo} alt="logo" />
-            </NavLink>
-          </div>
+          <div className="nav_items" id={showLinks ? 'hidden' : ''}>
+            <div className="navbar_left">
+              <NavLink className="nav__logo" to="/">
+                <img src={logo} alt="logo" />
+              </NavLink>
+            </div>
 
-          <div className="navbar_right">
-            <div
-              className="header__links"
-              id={showLinks ? 'hidden' : ''}
-              onClick={() => setShowlinks(!showLinks)}
-            >
-              <NavLink to="/" className="header__link">
-                Doctors
-              </NavLink>
-              <NavLink to="/book-appointment" className="header__link">
-                Book Appointment
-              </NavLink>
-              <NavLink to="/appointments" className="header__link">
-                Appointments
-              </NavLink>
-              <NavLink to="/add-new-doctor" className="header__link">
-                Add Doctor
-              </NavLink>
-              <NavLink to="/delete-doctor" className="header__link">
-                Delete Doctor
-              </NavLink>
+            <div className="navbar_right">
+              <div
+                className="header__links"
+                id={showLinks ? 'hidden' : ''}
+                onClick={() => setShowlinks(!showLinks)}
+              >
+                <NavLink to="/" className="header__link">
+                  Doctors
+                </NavLink>
+                <NavLink to="/book-appointment" className="header__link">
+                  Book Appointment
+                </NavLink>
+                <NavLink to="/appointments" className="header__link">
+                  Appointments
+                </NavLink>
+                <NavLink to="/add-new-doctor" className="header__link">
+                  Add Doctor
+                </NavLink>
+                <NavLink to="/delete-doctor" className="header__link">
+                  Delete Doctor
+                </NavLink>
+              </div>
             </div>
           </div>
         </nav>
