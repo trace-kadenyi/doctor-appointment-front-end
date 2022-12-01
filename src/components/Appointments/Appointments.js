05a5a6639eb579e06ab90fd850a18e2fb6c80355
Abby Nyakara <abbynyakara@gmail.com>
2022-12-01 17:18:10 +0300

    add appointment basic structure

--- src/components/Appointments/Appointments.js
+++ src/components/Appointments/Appointments.js
@@ -1,3 +1,4 @@
+/* eslint-disable array-callback-return */
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchAppointments, appointmentsSelector } from '../../Redux/AppointmentsSlice';
@@ -16,6 +17,27 @@ const Appointments = () => {
     <div className="appointments">
       <div className="header">
         <h2>My appointments</h2>
+        {/* Include doctor's info? */}
+        {
+          appointments && appointments.map((appointment) => {
+            <div className="appointment__card" key={appointment.id}>
+              <p>
+                Appointments:
+                {appointment.description}
+              </p>
+              <p>
+                Date of Appointment:
+                {appointment.date_of_appointment}
+              </p>
+              <p>
+                Time of Appointment:
+                {appointment.time_of_appointment}
+              </p>
+              {/* Add Icon to delete instead of a button */}
+              <button className="delete_appointment" type="button">Delete Appointment</button>
+            </div>;
+          })
+        }
       </div>
     </div>
   );
