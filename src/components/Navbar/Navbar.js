2ef844982963d08f24e9358c7bc32f46de86dd6c
Abby Nyakara <abbynyakara@gmail.com>
2022-12-01 11:12:28 +0300

    add responsiveness

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,43 +1,54 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import logo from '../../assets/images/logo.png';
 import humburgerMenu from '../../assets/images/humburger-menu.svg';
 import './navbar.css';
 
-const Navbar = () => (
-  <div className="navbar">
-    <nav className="navbar_main">
-      <div className="navbar_left">
-        <NavLink className="nav__logo" to="/">
-          <img src={logo} alt="logo" />
-        </NavLink>
-      </div>
+const Navbar = () => {
+  const [showLinks, setShowlinks] = useState(false);
 
-      <div className="navbar_right">
-        <div className="header__links">
-          <NavLink to="/" className="header__link">
-            Doctors
-          </NavLink>
-          <NavLink to="/book-appointment" className="header__link">
-            Book Appointment
-          </NavLink>
-          <NavLink to="/appointments" className="header__link">
-            Appointments
-          </NavLink>
-          <NavLink to="/add-new-doctor" className="header__link">
-            Add Doctor
-          </NavLink>
-          <NavLink to="/delete-doctor" className="header__link">
-            Delete Doctor
-          </NavLink>
-        </div>
+  return (
+    <div>
+      <div className="navbar">
+        <nav className="navbar_main">
+          <div className="navbar_left">
+            <NavLink className="nav__logo" to="/">
+              <img src={logo} alt="logo" />
+            </NavLink>
+          </div>
 
-        <button className="menu__button" type="button">
-          <img src={humburgerMenu} alt="Menu Icon" />
-        </button>
+          <div className="navbar_right">
+            <div
+              className="header__links"
+              id={showLinks ? 'hidden' : ''}
+              onClick={() => setShowlinks(!showLinks)}
+            >
+              <NavLink to="/" className="header__link">
+                Doctors
+              </NavLink>
+              <NavLink to="/book-appointment" className="header__link">
+                Book Appointment
+              </NavLink>
+              <NavLink to="/appointments" className="header__link">
+                Appointments
+              </NavLink>
+              <NavLink to="/add-new-doctor" className="header__link">
+                Add Doctor
+              </NavLink>
+              <NavLink to="/delete-doctor" className="header__link">
+                Delete Doctor
+              </NavLink>
+            </div>
+
+            <button className="menu__button" type="button">
+              <img src={humburgerMenu} alt="Menu Icon" />
+            </button>
+          </div>
+        </nav>
       </div>
-    </nav>
-  </div>
-);
+    </div>
+  );
+};
 
 export default Navbar;
