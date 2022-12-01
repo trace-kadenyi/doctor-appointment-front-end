ae53d2c70b58a0642271543ad40de62503612568
Aleksandra Ujvari <ujvari65@gmail.com>
2022-12-01 17:22:03 +0100

    Import Dr details

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import User from './components/Authentication/User';
 import { setCurrentUser } from './Redux/UserReducer';
 import DoctorsList from './components/Doctors/DoctorsList';
 import Navbar from './components/Navbar/Navbar';
+import DrDetail from './components/DoctorDetail/DrDetail';
 import 'react-toastify/dist/ReactToastify.css';
 import ProtectRoutes from './components/ProtectRoutes';
 
