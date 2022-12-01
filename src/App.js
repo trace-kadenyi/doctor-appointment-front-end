87d385e871b373abef46e13b86b973caea352d31
Abby Nyakara <81410040+AbbyNyakara@users.noreply.github.com>
2022-12-01 14:30:21 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar/Navbar';
 import { useDispatch } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 import Home from './components/Home';
 import User from './components/Authentication/User';
 import { setCurrentUser } from './Redux/UserReducer';
 import DoctorsList from './components/Doctors/DoctorsList';
+import Navbar from './components/Navbar/Navbar';
 import 'react-toastify/dist/ReactToastify.css';
 import ProtectRoutes from './components/ProtectRoutes';
 
