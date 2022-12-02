7bb4cf39e92063cc127d7242b27f3d1ac057a68f
brahimdidi <brahimgb1team@gmail.com>
2022-12-02 14:52:40 +0000

    rename home to be about

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+const About = () => {
+  const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+  return (
+    <section>
+       (<h1 className="text-danger">Check your internet Again</h1>)
+      {!currentUser && <h1>This is about page</h1> }
+    </section>
+  );
+};
+
+export default About;
+
+
