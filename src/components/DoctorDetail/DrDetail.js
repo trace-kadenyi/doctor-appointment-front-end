ef00721599ddc42581628aa156858786d3efc1a0
brahimdidi <brahimgb1team@gmail.com>
2022-12-03 21:37:37 +0000

    fix bug when doctor is undefined, use selectDoctor.

--- src/components/DoctorDetail/DrDetail.js
+++ src/components/DoctorDetail/DrDetail.js
@@ -2,64 +2,77 @@ import React, { useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { BiLeftArrow } from 'react-icons/bi';
+import { fetchDoctor, doctorSelector, selectDoctorsloading, selectDoctor } from '../../Redux/doctorSlice';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Card from 'react-bootstrap/Card';
 import Table from 'react-bootstrap/Table';
 import Container from 'react-bootstrap/Container';
 import './DrDetail.css';
+import preloader from '../../assets/images/preloader.gif';
 
-import { fetchDoctor, doctorSelector } from '../../Redux/doctorSlice';
 
 const DrDetail = () => {
   const dispatch = useDispatch();
-  const { doctor } = useSelector(doctorSelector);
+  const loading = useSelector(selectDoctorsloading);
   const { id } = useParams();
-  // const doctor = doctors.find((doc) => doc.id === parseInt(id, 10));
-
+  
   useEffect(() => {
+    console.log('doctor show run')
     dispatch(fetchDoctor(id));
-  }, [dispatch]);
+  }, []);
 
+  const doctor = useSelector(selectDoctor) || {}
+  
   return (
-    <Container>
-      <Row className="d-flex">
-        <Col xs={12} md={5}>
-          <Card className="mb-4 p-3">
-            <Card.Img variant="top" src={doctor.photo} alt="doctor" />
-          </Card>
-        </Col>
+    <>
+    { loading &&
+    <div className="loading">
+        <img src={preloader} alt="loading" className="preloader" />
+    </div>
+    }
+    {doctor.id &&
+      <Container>
+        <Row className="d-flex">
+          <Col xs={12} md={5}>
+            <Card className="mb-4 p-3">
+              <Card.Img variant="top" src={doctor.photo} alt="doctor" />
+            </Card>
+          </Col>
 
-        <Col xs={12} md={5}>
-          <Card.Body>
-            <Card.Title><h5>{doctor.name}</h5></Card.Title>
-            <div>
-              <Table striped bordered hover responsive="sm">
-                <tbody>
-                  <tr>
-                    <th>Specialization</th>
-                    <td>{doctor.specialization}</td>
-                  </tr>
-                  <tr>
-                    <th>Bio</th>
-                    <td>{}</td>
-                  </tr>
-                  <tr>
-                    <th>Appointment</th>
-                    <td className="d-flex justify-content-end"><Link to={`/doctors/${id}/appointments`} className="btn btn-success">Book Appointment</Link></td>
-                  </tr>
-                </tbody>
-              </Table>
-            </div>
-          </Card.Body>
-        </Col>
-      </Row>
-      <button type="button" className="arrow left">
-        <Link className="back" to="/">
-          <BiLeftArrow />
-        </Link>
-      </button>
-    </Container>
+          <Col xs={12} md={5}>
+            <Card.Body>
+              <Card.Title><h5>{doctor.name}</h5></Card.Title>
+              <div>
+                <Table striped bordered hover responsive="sm">
+                  <tbody>
+                    <tr>
+                      <th>Specialization</th>
+                      <td>{doctor.specialization}</td>
+                    </tr>
+                    <tr>
+                      <th>Bio</th>
+                      <td>{}</td>
+                    </tr>
+                    <tr>
+                      <th>Appointment</th>
+                      <td className="d-flex justify-content-end"><Link to={`/doctors/${id}/appointments`} className="btn btn-success">Book Appointment</Link></td>
+                    </tr>
+                  </tbody>
+                </Table>
+              </div>
+            </Card.Body>
+          </Col>
+        </Row>
+        <button type="button" className="arrow left">
+          <Link className="back" to="/">
+            <BiLeftArrow />
+          </Link>
+        </button>
+      </Container>
+    }
+    {(!loading && !doctor.id) && <div className='alert alert-danger'>something went wrond</div>}
+    </>
   );
 };
 
