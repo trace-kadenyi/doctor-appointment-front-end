f5bd1873f78d074d7e35cab4282817db36dd6574
Abby Nyakara <abbynyakara@gmail.com>
2022-12-05 17:39:00 +0300

    add changes

--- src/components/DisplayAppointments/Display.js
+++ src/components/DisplayAppointments/Display.js
@@ -2,12 +2,13 @@
 import React, { useEffect } from 'react';
 import './Display.css';
 import { useSelector, useDispatch } from 'react-redux';
-import { appointmentsSelector, fetchAppointments } from '../../Redux/AppointmentsSlice';
+import { fetchAppointments } from '../../Redux/AppointmentsSlice';
 
 const Display = () => {
   const dispatch = useDispatch();
   const appointments = useSelector((state) => state.appointments);
   const allDoctors = useSelector((state) => state.doctors);
+
   // Fetch the appointments for the curent user:
   // loop through the appointments and render each appointment
 
