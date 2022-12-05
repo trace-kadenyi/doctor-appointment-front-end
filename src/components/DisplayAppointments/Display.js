78a1b7dd8da290161a1e26979ad1eee58c40ee90
Abby Nyakara <abbynyakara@gmail.com>
2022-12-05 13:01:19 +0300

    loop appointments

--- src/components/DisplayAppointments/Display.js
+++ src/components/DisplayAppointments/Display.js
@@ -17,30 +17,30 @@ const Display = () => {
   return (
     <div className="display">
       <h1 className="display__header">Booked Appointments</h1>
-
-
-      <div className="appointment__card">
-        <div className="top">
-          <div className="left">
-            <p>Appointment Date:</p>
-            {/* should destroy the appointment */}
-            <p>Delete</p>
+      {
+        appointments && appointments.map((appointment) => (
+          <div className="appointment__card">
+            <div className="top">
+              <div className="left">
+                <p>Appointment Date:</p>
+                {/* should destroy the appointment */}
+                <p>Delete</p>
+              </div>
+              <div className="time">
+                Monday Dec 05, 2022 at 8.30am
+              </div>
+            </div>
+            <div className="bottom">
+              <p>
+                Dr Kadenyi
+                <br />
+                Opthamologist
+              </p>
+            </div>
           </div>
-          <div className="time">
-            Monday Dec 05, 2022 at 8.30am
-          </div>
-        </div>
-        <div className="bottom">
-          <p>
-            Dr Kadenyi
-            <br />
-            Opthamologist
-          </p>
-        </div>
-
-        
+        ));
       </div>
-    </div>
+    }      
   );
 };
 
