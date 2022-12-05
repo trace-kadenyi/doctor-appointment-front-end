1fd448d47e8d0e892b1e70cf8b82c1df49c7c2b7
Abby Nyakara <abbynyakara@gmail.com>
2022-12-05 14:14:02 +0300

    add delete appoinmetn reducer

--- src/Redux/delete-appointments.js
+++ src/Redux/delete-appointments.js
@@ -3,15 +3,25 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 const REMOVE_APPOINTMENT = 'appointments/REMOVE_APPOINTMENT';
 const initialState = [];
 
-export const deleteAppointment = createAsyncThunk(
-  REMOVE_APPOINTMENT,
-  async (data) => {
-    await fetch(`http://localhost:3000/api/v1/appointments/{appointment_id}`, {
-      method: 'DELETE',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: data.token,
-      },
-    });
+// const deleteAppointment = createAsyncThunk(
+//   REMOVE_APPOINTMENT,
+//   async (data) => {
+//     await fetch('http://localhost:3000/api/v1/appointments/{appointment_id}', {
+//       method: 'DELETE',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         Authorization: data.token,
+//       },
+//     });
+//   },
+// );
+
+const deleteAppointmentSlice = createSlice({
+  name: 'removeAppointment',
+  initialState,
+  reducers: {
+    deleteAppointment: (state) => state,
   },
-);
+});
+
+export default deleteAppointmentSlice.reducer;
