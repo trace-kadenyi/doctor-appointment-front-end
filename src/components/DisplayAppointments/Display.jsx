a87e3c8e4a9f5103eacecfb010d0b5ba4f6d396a
Abby Nyakara <abbynyakara@gmail.com>
2022-12-05 11:53:34 +0300

    add appointments ui

---
+++ src/components/DisplayAppointments/Display.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './Display.css';
+
+const Display = () => {
+  return (
+    <div className="display">
+      <h1 className='display__header'>Booked Appointments</h1>
+      <div className="appointment__card">
+        <div className="top">
+          <div className="left">
+            <p>Appointment Date:</p>
+            <a href="">Delete</a>
+          </div>
+          <div className="time">
+            Monday Dec 05, 2022 at 8.30am
+          </div>
+        </div>
+        <div className="bottom">
+          <p>
+            Dr Kadenyi <br />
+            Opthamologist
+          </p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Display
