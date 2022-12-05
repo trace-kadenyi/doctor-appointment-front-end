6087a5e9abdcea59df2241394f811f93e601fa9a
Abby Nyakara <abbynyakara@gmail.com>
2022-12-05 12:21:49 +0300

    correct linters

--- src/components/DisplayAppointments/Display.js
+++ src/components/DisplayAppointments/Display.js
@@ -2,29 +2,29 @@ import React from 'react';
 import './Display.css';
 import { useSelector, useDispatch } from 'react-redux';
 
-const Display = () => {
-  return (
-    <div className="display">
-      <h1 className='display__header'>Booked Appointments</h1>
-      <div className="appointment__card">
-        <div className="top">
-          <div className="left">
-            <p>Appointment Date:</p>
-            <a href="">Delete</a>
-          </div>
-          <div className="time">
-            Monday Dec 05, 2022 at 8.30am
-          </div>
+const Display = () => (
+  <div className="display">
+    <h1 className="display__header">Booked Appointments</h1>
+    <div className="appointment__card">
+      <div className="top">
+        <div className="left">
+          <p>Appointment Date:</p>
+          <a href="#">Delete</a>
         </div>
-        <div className="bottom">
-          <p>
-            Dr Kadenyi <br />
-            Opthamologist
-          </p>
+        <div className="time">
+          Monday Dec 05, 2022 at 8.30am
         </div>
       </div>
+      <div className="bottom">
+        <p>
+          Dr Kadenyi
+          {' '}
+          <br />
+          Opthamologist
+        </p>
+      </div>
     </div>
-  )
-}
+  </div>
+);
 
-export default Display
+export default Display;
