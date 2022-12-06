82e096d804d92aa3aa64828b373c7dc5847fc2b2
kadenyi <treykadenyi@gmail.com>
2022-12-06 14:19:20 +0300

    Implement the addAppointment method and edit the form input fields

--- src/components/BookAppointments/Appointments.js
+++ src/components/BookAppointments/Appointments.js
@@ -1,41 +1,77 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable max-len */
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
 import './Appointments.css';
+import { selectCurrentUser } from '../../Redux/UserReducer';
+import { doctorSelector } from '../../Redux/doctorSlice';
+import { addAppointment } from '../../Redux/AppointmentsSlice';
 
 const Appointments = () => {
-  // const allDoctors = useSelector((state) => state.doctors);
+  const { doctors } = useSelector(doctorSelector);
+  const currentUser = useSelector(selectCurrentUser);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [appointment, setAppointment] = useState({
+    doctorId: '',
+    dateOfAppointment: '',
+    timeOfAppointment: '',
+    description: '',
+    userId: currentUser.id,
+  });
 
-  const bookAppointment = (e) => {
+  // handle the change of the input fields
+  const handleChange = (e) => {
+    setAppointment({ ...appointment, [e.target.name]: e.target.value });
+  };
+
+  // handle the submission of the form
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const formObject = new FormData(e.target);
-    const data = Object.fromEntries(formObject.entries());
-    // Form Object wil give a key value pair
-    // dispatch an action to send data
+    dispatch(addAppointment(appointment));
+    navigate('/appointments');
   };
 
   return (
     <div className="appointments">
       {/* <h1>Book your appointment</h1> */}
       <div className="appointments__tab">
-        <form action="" className="book__doctor" onSubmit={bookAppointment}>
+        <form action="" className="book__doctor" onSubmit={handleSubmit}>
+          {/* date and time */}
           <div className="date__selector">
-            <label htmlFor="date">Select a date and time</label>
-            <input type="datetime-local" name="date_time" className="date" />
+            <label htmlFor="date">Date of Appointment</label>
+            <input type="date" name="dateOfAppointment" className="date" onChange={handleChange} />
           </div>
+          <div className="date__selector">
+            <label htmlFor="time">Time of Appointment</label>
+            <input type="time" name="timeOfAppointment" className="time" onChange={handleChange} />
+          </div>
+          {/* doctor */}
           <div className="doctor__selector">
-            <label htmlFor="Doctor">Select Doctor</label>
-            <select name="doctor_select">
+            <label htmlFor="Doctor">Select a Doctor</label>
+            <select name="doctor_select" onChange={handleChange}>
               <option value="no-value">Doctor</option>
-              {/* {allDoctors.map((doctor) => (<option key={doctor.id} value={doctor.id}>{`Dr. ${doctor.name}`}</option>))} */}
+              {/* map through doctors */}
+              {doctors.map((doctor) => (
+                <option key={doctor.id} value={doctor.id} name="doctorId">
+                  {doctor.name}
+                </option>
+              ))}
             </select>
           </div>
-
-          <button type="submit" className="book_button">Book Now</button>
+          {/* description */}
+          <div className="description">
+            <label htmlFor="description">Description</label>
+            <input name="description" id="" cols="30" rows="10" onChange={handleChange} />
+          </div>
+          <button type="submit" className="book_button">
+            Book Now
+          </button>
         </form>
       </div>
-
     </div>
   );
 };
