d4839bb22b632aa5a823785404a4183a7e032543
brahimdidi <brahimgb1team@gmail.com>
2022-12-06 10:10:21 +0000

    fetch currentUser appointments , fix bug in fetching doctor for appointment

--- src/components/DisplayAppointments/Display.js
+++ src/components/DisplayAppointments/Display.js
@@ -4,29 +4,34 @@ import './Display.css';
 import { useSelector, useDispatch } from 'react-redux';
 import { fetchAppointments, selectAppointments } from '../../Redux/AppointmentsSlice';
 import { fetchDoctors, selectDoctors, selectDoctorsFulfilled } from '../../Redux/doctorSlice';
+import { selectCurrentUser } from '../../Redux/UserReducer';
 
 const Display = () => {
   const dispatch = useDispatch();
   const appointments = useSelector(selectAppointments);
   const doctorsFulfilled = useSelector(selectDoctorsFulfilled);
+  const currentUser = useSelector(selectCurrentUser);
+
   // Fetch the appointments for the curent user:
   // loop through the appointments and render each appointment
 
   useEffect(() => {
-    dispatch(fetchAppointments());
+    dispatch(fetchAppointments({userId: currentUser.id}));
     dispatch(fetchDoctors());
   }, [dispatch]);
 
   const allDoctors = useSelector(selectDoctors);
-
+  let findDoctorName  = (id) => { return allDoctors.find(e => e.id == id).name; };
+  let findDoctorSpecialization  = (id) => { allDoctors.find(e => e.id == id).specialization; };
   return (
 
     <div className="display">
       <h1 className="display__header">Booked Appointments</h1>
 
       {
-        (appointments && doctorsFulfilled) && appointments.map((appointment, index) => (
-          <div className="appointment__card" key={index}>
+        (appointments && doctorsFulfilled) && appointments.map((appointment) => (
+          <>
+          <div className="appointment__card" key={appointment.id}>
             <div className="top">
               <div className="left">
                 <p>Appointment Date:</p>
@@ -43,12 +48,13 @@ const Display = () => {
             </div>
             <div className="bottom">
               <p>
-                {allDoctors[appointment.doctor_id].name}
+                {findDoctorName(appointment.doctor_id)}
                 <br />
-                {allDoctors[appointment.doctor_id].specialization}
+                {findDoctorSpecialization(appointment.doctor_id)}
               </p>
             </div>
           </div>
+          </>
         ))
       }
     </div>
